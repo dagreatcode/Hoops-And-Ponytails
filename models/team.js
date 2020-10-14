3a9e9a3d3a85b2e1692070bb9b4b244e883e2958
Phantom <fay.ashbro@gmail.com>
2020-10-14 18:33:56 -0400

    integer mispelled

--- models/team.js
+++ models/team.js
@@ -5,27 +5,27 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.STRING
         }, 
         player_number: {
-            type: DataTypes.INTERGER
+            type: DataTypes.INTEGER
         }, 
         api_player_id: {
             type: DataTypes.STRING
         }, 
         player_id:{
-            type: DataTypes.INTERGER
+            type: DataTypes.INTEGER
         }, 
         username: {
             type: DataTypes.STRING
         } 
     });
 
-    team_name.associate = function(models) {
+    // Team.associate = function(models) {
       
-        Team.belongsTo(models.Player, {
-          foreignKey: {
-            allowNull: true
-          }
-        });
-      };
+    //     Team.belongsTo(models.Player, {
+    //       foreignKey: {
+    //         allowNull: true
+    //       }
+    //     });
+    //   };
     
       return Team;
 
