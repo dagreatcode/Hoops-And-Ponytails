c5e6fdebbe4f47abd819ee941f275b820527ca3e
Andrew0502 <agcmarcus@gmail.com>
2020-10-14 21:22:18 -0400

    Added allowInsecurePrototypeAccess to server.js alongside require variables for the api-routes and html-routes.

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const exphbs = require("express-handlebars");
+const {
+    allowInsecurePrototypeAccess,
+  } = require("@handlebars/allow-prototype-access");
 const app = express();
 const db = require("./models");
+const apiRoutes = require("./routes/api-routes.js");
+const htmlRoutes = require("./routes/html-routes.js");
 
 const PORT = process.env.PORT || 8080;
 
@@ -9,8 +14,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.engine("handlebars", exphbs({ defaultLayout: "main", 
+handlebars: allowInsecurePrototypeAccess(handlebars), }));
 app.set("view engine", "handlebars");
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
 
 // views routes
 app.get("/", (req, res) => {
