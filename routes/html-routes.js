007d8d68bc979aa55364f328c36c73ded4e1b168
Phantom <fay.ashbro@gmail.com>
2020-10-15 09:31:41 -0400

    figuring out view routes last night

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,27 +2,47 @@ const express = require("express");
 const router = express.Router();
 const db = require("../models");
 const path = require("path");
+const app = express();
 
 // VIEWS ROUTES
+module.export = function(app){
+  app.get("/", (req, res) => {
+    res.redirect("index")
+  });
 
-router.get("/", (req, res) => {
-  if(req.email) {
-      res.redirect("/home");
-  }
-  res.sendFile(path.join(__dirname, "../views/home.handlebars"));
-});
+  app.get("/editteams", (req,res)=> {
+    res.render("");
+    // res.send("works");
+  })
+};
 
-router.get("/index", (req, res) => {
-  res.sendFile(Path.join(__dirname, "../views/index.handlebars"))
-});
+  // 
+  // if(req.email) {
+      
+  // }
 
-router.get("/editteams", (req, res) => {
-    res.sendFile(Path.join(__dirname, "../views/editteams.handlebars"))
-});
+  
+  // app.get("/", function(req, res) {
+  //   ;
+  // });
+  // res.redirect(path.join(__dirname, "../views/home.handlebars"))
+  // res.sendFile()
+  // res.redirect("/home");
+  // console.log("maybe worked")
+  // res.sendFile(path.join(__dirname, "../views/home.handlebars"));
 
-  router.get("/teamchallenge", (req, res) => {
-    res.sendFile(Path.join(__dirname, "../views/teamchallenge.handlebars"))
-});
 
-module.exports = router;
+// router.get("/index", (req, res) => {
+//   res.sendFile(Path.join(__dirname, "../views/index.handlebars"))
+// });
+
+// router.get("/editteams", (req, res) => {
+//     res.sendFile(Path.join(__dirname, "../views/editteams.handlebars"))
+// });
+
+//   router.get("/teamchallenge", (req, res) => {
+//     res.sendFile(Path.join(__dirname, "../views/teamchallenge.handlebars"))
+// });
+
+// module.exports = router;
 
