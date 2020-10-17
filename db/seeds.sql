c0c7620189e79966b8b9f18dfdd13bef6c284b39
Phantom <fay.ashbro@gmail.com>
2020-10-16 21:09:34 -0400

    took out palyer seed data

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,39 +1,3 @@
-INSERT INTO player 
-
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Breanna Stewart", , 19.7)
-
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Arike Ogunbowale",24 , 22.8)
-
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("A'Ja Wilson",21 , 20.5)
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Dewanna Bonner",24 , 19.7)
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Diana Taurasi",3 , 18.7)
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Sabrina Ionescu",20 , 18.3)
-
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Kelsey Mitchell",0 , 17.9)
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Skylar Diggins-Smith",4 , 17.7)
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Brittney Griner",42 , 17.7)
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Chennedy Carter", 3, 17.4)
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Betnijah Laney", 44, 17.2)
-INSERT INTO player ( player_name, jersey_number,season_points)  
-VALUES ("Myisha Hines-Allen", 2, 17.2)
-
 
 
 INSERT INTO user ( usernamename)  
