ffe02a6bdbd64952de6fde827a15f44560a5cc30
Phantom <fay.ashbro@gmail.com>
2020-10-18 14:51:52 -0400

    changes to signup link

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -37,10 +37,6 @@ module.exports = function(app){
   });
   app.get("/index", (req, res) => {
     res.render("index")
-    // do I need both this and the app.get for "/" ?
-  });
-  app.get("/signup", (req, res) => {
-    res.render("signup")
   });
 
   // //build your team 
@@ -57,75 +53,6 @@ module.exports = function(app){
         });
   });
 
-
-
-
-  //   //api call to recieve list of player 
-  //   // axios.get(
-  //   //     "http://api.sportradar.us/wnba/trial/v7/en/league/hierarchy.json?api_key=226774v99bae546n3skmrm3a"
-
-  //     ).then(function (res) {
-  //       // console.log(res.data)
-  //       var teams = [];
-
-  //       var conferences = res.data.conferences
-  //       for (var i = 0; i < conferences.length; i++) {
-  //         for (var t = 0; t < conferences[i].teams.length; t++) {
-  //           teams.push({
-  //             teamId: conferences[i].teams[t].id,
-  //             teamName: conferences[i].teams[t].name
-  //           })
-  //         }
-  //       }
-  //       return teams
-  //     }).then(function (teams) {
-       
-  //       var index = 0;
-  //       var players = [];
-  //       console.log(teams[index]);
-  //       function getData() {
-  //         var url = "http://api.sportradar.us/wnba/trial/v7/en/teams/" + teams[index].teamId + "/profile.json?api_key=226774v99bae546n3skmrm3a";
-  //         axios.get(url).then(function (res) {
-  //           var teamRoster = res.data.players;
-  //           for (var p = 0; p < teamRoster.length; p++) {
-  //             players.push({
-  //               playerId: teamRoster[p].id,
-  //               playerName: teamRoster[p].full_name,
-  //               jerseyNumber: teamRoster[p].jersey_number,
-  //               position: teamRoster[p].position,
-  //               exp: teamRoster[p].experience
-  //             })
-  //           };
-  //           return players
-  //         }).then(function (players) {
-  //           setTimeout(() => {
-  //             index++;
-  //             if (index < teams.length - 1) {
-  //               console.log("true")
-  //               getData();
-  //             } else {
-  //               console.log("false")
-  //               console.log(players)
-                //res.render("buildyourteam",{
-  //                 players: players
-  //               })
-  //               // console.log(players);
-  //               // res.send(players);
-  //               // console.log("false")
-  //             }
-  //           }, 1000)
-
-  //         }).catch((err) => {
-  //           if (err) {
-  //             console.log(err);
-  //             console.log("error in players call")
-  //           };
-  //         });
-  //       }
-  //       setTimeout(getData,1000)
-        
-  //     });
-    
  
   app.get("/editteams", (req,res) => {
     res.render("editteams")
@@ -137,22 +64,8 @@ module.exports = function(app){
   app.get("/home", (req,res) => {
     res.render("home")
   });
+  app.get("/signup", (req,res) => {
+    res.render("signup");
+  })
 };
 
-
-// setTimeout(() => {
-//   index++;
-//   if (index < teams.length - 1) {
-//     console.log("true")
-//     getData();
-//   } else {
-//     console.log("false")
-//     console.log(players)
-//     res.render("buildyourteam",{
-//       players: players
-//     })
-//     // console.log(players);
-//     // res.send(players);
-//     // console.log("false")
-//   }
-// }, 1000)
